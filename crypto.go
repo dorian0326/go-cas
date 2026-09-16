0a142757bac3e6a8fac955a9b2b913511425be88
dorian0326 <225304025+dorian0326@users.noreply.github.com>
2026-09-16 15:17:35 +0800

    v 0.1.0 finished

---
+++ crypto.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+)
+
+// generateID returns a random ID string of 32 bytes
+// used as FileServer.ID when none is set, and as the per-node directory name.
+func generateID() string {
+	buf := make([]byte, 32)
+	io.ReadFull(rand.Reader, buf)
+	return hex.EncodeToString(buf)
+}
+
+// hashKey returns the MD5 hash of the given key as a hex string
+// used for key derivation and authentication.
+func hashKey(key string) string {
+	hash := md5.Sum([]byte(key))
+	return hex.EncodeToString(hash[:])
+}
+
+// newEncryptionKey returns a random encryption key of 32 bytes
+// used for symmetric encryption of file contents.
+func newEncryptionKey() []byte {
+	keyBuf := make([]byte, 32)
+	io.ReadFull(rand.Reader, keyBuf)
+	return keyBuf
+}
+
+// copyStream encrypts and writes the contents of src to dst using the given cipher stream.
+func copyStream(stream cipher.Stream, blockSize int, src io.Reader, dst io.Writer) (int, error) {
+	var (
+		buf = make([]byte, 32*1024)
+		nw  = blockSize
+	)
+	for {
+		n, err := src.Read(buf)
+		if n > 0 {
+			stream.XORKeyStream(buf[:n], buf[:n])
+			nn, err := dst.Write(buf[:n])
+			if err != nil {
+				return 0, err
+			}
+			nw += nn
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+	return nw, nil
+}
+
+// copyDecrypt decrypts and writes the contents of src to dst using the given cipher stream.
+func copyDecrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return 0, err
+	}
+
+	// Read the IV from the given io.Reader and use it to create a cipher stream.
+	iv := make([]byte, 16)
+	if _, err := src.Read(iv); err != nil {
+		return 0, err
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	return copyStream(stream, block.BlockSize(), src, dst)
+}
+
+// copyEncrypt encrypts and writes the contents of src to dst using the given cipher stream.
+func copyEncrypt(key []byte, src io.Reader, dst io.Writer) (int, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return 0, err
+	}
+
+	iv := make([]byte, block.BlockSize()) // 16 bytes
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return 0, err
+	}
+
+	// prepend the IV to the file.
+	if _, err := dst.Write(iv); err != nil {
+		return 0, err
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	return copyStream(stream, block.BlockSize(), src, dst)
+}
--- p2p/encoding.go
+++ p2p/encoding.go
@@ -1,11 +1,18 @@
 package p2p
 
 import (
+	"encoding/gob"
 	"io"
 )
 
 type Decoder interface {
 	Decode(io.Reader, *RPC) error
+}
+
+type GOBDecoder struct{}
+
+func (dec GOBDecoder) Decode(r io.Reader, msg *RPC) error {
+	return gob.NewDecoder(r).Decode(msg)
 }
 
 type DefaultDecoder struct{}
